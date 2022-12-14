87278feb326517ea2d2995e4bdd599912219aa93
Karthik <karthikeyanmurugan393@gmail.com>
2022-12-14 22:26:48 +0530

    Updated the files

--- app/src/main/java/com/karthik/imagesearch/data/Constants.kt
+++ app/src/main/java/com/karthik/imagesearch/data/Constants.kt
@@ -4,5 +4,5 @@ package com.karthik.imagesearch.data
 const val BASE_URL: String =
     "https://api.flickr.com/services/rest/"
 
-
+const val PAGE_SIZE = 100
 
--- app/src/main/java/com/karthik/imagesearch/data/repository/ImageRepository.kt
+++ app/src/main/java/com/karthik/imagesearch/data/repository/ImageRepository.kt
@@ -1,11 +1,11 @@
 package com.karthik.imagesearch.data.repository
 
-import android.nfc.tech.MifareUltralight.PAGE_SIZE
 import androidx.lifecycle.LiveData
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.liveData
+import com.karthik.imagesearch.data.PAGE_SIZE
 import com.karthik.imagesearch.data.api.ImageSearchService
 import com.karthik.imagesearch.data.model.Photo
 import javax.inject.Inject
